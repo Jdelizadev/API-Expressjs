863d90ec73f4bf863821977a1b4be0187d05b637
Jaime De Lizaola <jaimedelizaola@gmail.com>
2025-10-25 19:22:50 -0600

    update v

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ const routes = require('./routes')
 const app = express()
 const errorHandler = require('./middleware/errorHandler')
 const LoggerMiddleware = require('./middleware/loggin')
+const cors = require('cors')
 
+const corsOptions = {
+    // Reemplaza 'http://localhost:5173' con la URL exacta de tu frontend de React
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Esto es necesario si manejas cookies o tokens de sesión
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(errorHandler)
 app.use(LoggerMiddleware)
